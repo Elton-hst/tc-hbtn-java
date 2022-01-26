fb0840321027e2ed97589333a9651115d39c91c1
Elton Henrique <eltonhst34@gmail.com>
2022-01-26 10:22:40 -0300

    teste

--- 0x00-java-hello_world/PrintFormatacaoNumeros.java
+++ 0x00-java-hello_world/PrintFormatacaoNumeros.java
@@ -1,3 +1,7 @@
 public class PrintFormatacaoNumeros {
-    
+    public static void main(String[] args) {
+        float taxa = 0.2456f;
+        float valor = 7654.321f;
+        System.out.printf("Valor: " + String.format("R$ %,.2f",valor) + "\nTaxa: %.2f%%\n", taxa);
+    }
 }
