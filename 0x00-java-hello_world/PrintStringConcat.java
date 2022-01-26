a65107c2950dce44c467f16234ec5ad96d18e28c
Elton Henrique <eltonhst34@gmail.com>
2022-01-26 02:32:38 -0300

    atualizando o repositorio

--- 0x00-java-hello_world/PrintStringConcat.java
+++ 0x00-java-hello_world/PrintStringConcat.java
@@ -1,6 +1,7 @@
 public class PrintStringConcat {
     public static void main(String[] args) {
-        String texto1 = "Holberton"; String texto2 = "School";
+        String texto1 = "Holberton"; 
+        String texto2 = "School";
         String texto3 = "Bem vindo a " + texto1 + " " + texto2 + "!";
         System.out.println(texto3); 
     }
