0d0ea23cbc711e688a89d600b703d6f2b491986b
Elton Henrique <eltonhst34@gmail.com>
2022-01-26 02:09:21 -0300

    atualizando o repositorio

--- 0x00-java-hello_world/PrintIntegerVariable.java
+++ 0x00-java-hello_world/PrintIntegerVariable.java
@@ -1,3 +1,11 @@
 public class PrintIntegerVariable {
     
+    public static void main(String[] args) {
+        
+        int numero = 329;
+
+        System.out.println("Avenida Andaló " + numero );
+
+    }
+
 }
