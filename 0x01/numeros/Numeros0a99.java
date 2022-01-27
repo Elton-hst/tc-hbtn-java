81c1b41693df3325d256ff96deb9aec04daeb87f
Elton Henrique <eltonhst34@gmail.com>
2022-01-27 00:32:50 -0300

    teste

--- 0x01/numeros/Numeros0a99.java
+++ 0x01/numeros/Numeros0a99.java
@@ -2,7 +2,11 @@ public class Numeros0a99 {
     public static void main(String[] args) {
         int numero;
         for (numero = 0; numero < 100; numero++) {
-            System.out.print(numero + ", ");
-        }
+            if (numero <= 98){
+                System.out.print(numero + ", ");
+            }else {
+                System.out.print(numero);
+            }
+        }   
     }
 }
