bb753ed7f30af1e61412dff6b2b2f7c373c2f5b2
Elton Henrique <eltonhst34@gmail.com>
2022-01-28 12:39:42 -0300

    teste

---
+++ 0x01/pares/NumerosPares.java
@@ -0,0 +1,12 @@
+public class NumerosPares {
+    public static void main(String[] args) {
+        int numero;
+        for (numero = 0; numero <= 98; numero++){
+            if (numero % 2 == 0 && numero < 98){
+                System.out.print(numero + ", ");
+            }else if (numero == 98) {
+                System.out.print(numero);
+            }
+        }
+    }
+}
