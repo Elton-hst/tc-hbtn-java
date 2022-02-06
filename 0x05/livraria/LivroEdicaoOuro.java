ce976df32171f6148a5fce79d20f61e305c584b2
Elton Henrique <eltonhst34@gmail.com>
2022-02-06 01:54:05 -0300

    teste

--- 0x05/livraria/LivroEdicaoOuro.java
+++ 0x05/livraria/LivroEdicaoOuro.java
@@ -5,9 +5,11 @@ public class LivroEdicaoOuro extends Livro {
 	
 	public LivroEdicaoOuro(String titulo, String autor, double preco) throws LivroInvalidoException, AutorInvalidoException {
 		super(titulo, autor, preco) ;
+		// TODO Auto-generated constructor stub
 	}
 
 	public double getPreco() {
 		return preco += preco*0.30f;
-    }
+	}
+
 }
