3499d36a90e20881272c0053cfaeae130adb87a0
Elton Henrique <eltonhst34@gmail.com>
2022-02-06 02:30:00 -0300

    teste

---
+++ 0x05/conta_basica/exceptions/OperacaoInvalidaException.java
@@ -0,0 +1,10 @@
+package exceptions;
+
+public class OperacaoInvalidaException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+	public OperacaoInvalidaException(String msg){
+        super(msg);
+    }
+}
