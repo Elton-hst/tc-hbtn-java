94cae7c2bbd0016498a40613345edfc1e335e30a
Elton Henrique <eltonhst34@gmail.com>
2022-02-14 11:23:31 -0300

    teste

---
+++ 0x10/numeros/ManipularArrayNumeros.java
@@ -0,0 +1,33 @@
+import java.util.List;
+
+public class ManipularArrayNumeros {
+
+    public static int buscarPosicaoNumero(List<Integer> numeros, int num) {
+        return numeros.indexOf(num);
+    }
+    
+    public static void adicionarNumero(List<Integer> numeros, int num) {
+        if(buscarPosicaoNumero(numeros, num) != -1) {
+            throw new IllegalArgumentException("Numero jah contido na lista");
+        }else {
+            numeros.add(num);
+        }
+    }
+
+    public static void removerNumero(List<Integer> numeros, int num) {
+        if(buscarPosicaoNumero(numeros, num) == -1) {
+            throw new IllegalArgumentException("Numero nao encontrado na lista");
+        }else {
+            numeros.remove(num);
+        }
+    }
+
+    public static void substituirNumero(List<Integer> numeros, int numeroSubstituir, int numeroSubstituto) {
+		if (buscarPosicaoNumero(numeros, numeroSubstituir) == -1) {
+			numeros.add(numeroSubstituto);
+		}else {
+			numeros.set(buscarPosicaoNumero(numeros, numeroSubstituir), numeroSubstituto);
+		}
+	}
+
+}
