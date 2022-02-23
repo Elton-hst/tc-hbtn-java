f488e368b5452a5b3ca62b78c791604a5be1d3e1
Elton Henrique <eltonhst34@gmail.com>
2022-02-23 15:14:05 -0300

    teste

--- 0x14/duplicados/NumerosDuplicados.java
+++ 0x14/duplicados/NumerosDuplicados.java
@@ -1,3 +1,29 @@
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
 public class NumerosDuplicados {
-    
+
+    public static TreeSet<Integer> buscar(int[] inteiros) {
+
+        TreeSet<Integer> numeros = new TreeSet<>();
+        Set<Integer> inteirosSet = new HashSet<>();
+
+        for (int inteiro : inteiros) {
+            inteirosSet.add(inteiro);
+        }
+
+        for (Integer num : inteirosSet) {
+            int count = 0;
+            for (Integer num2 : inteiros) {
+                if (num.equals(num2)){
+                    count ++;
+                }
+            }
+            if (count >= 2){
+                numeros.add(num);
+            }
+        }
+        return numeros;
+    }
 }
