fbeb8885c4a87c74932f8663eea9e5eda4cb7850
Elton Henrique <eltonhst34@gmail.com>
2022-02-24 14:46:47 -0300

    teste

--- 0x14/telefones_duplicados/Telefone.java
+++ 0x14/telefones_duplicados/Telefone.java
@@ -1,3 +1,6 @@
+
+import java.util.Objects;
+
 public class Telefone {
 
     private String codigoArea;
@@ -19,5 +22,22 @@ public class Telefone {
     @Override
     public String toString() {
         return "(" + codigoArea + ") " + numero;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Telefone telefone = (Telefone) o;
+        return Objects.equals(codigoArea, telefone.codigoArea) && Objects.equals(numero, telefone.numero);
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 7;
+        result = prime * result + ((codigoArea == null) ? 0 : codigoArea.hashCode());
+        result = prime * result + ((numero == null) ? 0 : numero.hashCode());
+        return result;
     }
 }
