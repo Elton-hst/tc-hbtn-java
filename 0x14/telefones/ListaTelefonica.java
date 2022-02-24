ae160852cc9a190837450efbd6c9b1861f2b0ffb
Elton Henrique <eltonhst34@gmail.com>
2022-02-24 14:12:32 -0300

    teste

--- 0x14/telefones/ListaTelefonica.java
+++ 0x14/telefones/ListaTelefonica.java
@@ -1,3 +1,30 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class ListaTelefonica {
+    
+    private HashMap<String, ArrayList<Telefone>> hashMapTelefone;
+
+    public ListaTelefonica() {
+        this.hashMapTelefone = new HashMap<>();
+    }
+
+
+    public void adicionarTelefone(String nome, Telefone telefone) {
+        ArrayList<Telefone> telefones = hashMapTelefone.get(nome);
+
+        if(telefones == null) {
+            telefones = new ArrayList<>();
+            telefones.add(telefone);
+            hashMapTelefone.put(nome, telefones);
+        } 
+        else {
+            telefones.add(telefone);
+        }
+    } 
+
+    public ArrayList<Telefone> buscar(String nome) {
+        return hashMapTelefone.get(nome);
+    }
     
 }
