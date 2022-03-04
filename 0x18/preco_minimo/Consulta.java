0e722255cb9173ad44cf0ac087e97eb40de6e9ff
Elton Henrique <eltonhst34@gmail.com>
2022-03-04 15:50:58 -0300

    teste

---
+++ 0x18/preco_minimo/Consulta.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Consulta {
+
+    public static List<Produto> obterLivrosDoPedido(Pedido pedido) {
+        return pedido.getProdutos().stream()
+                .filter(p -> p.getCategoria() == CategoriaProduto.LIVRO)
+                .collect(Collectors.toList());
+    }
+
+    public static Produto obterProdutoMaiorPreco(List<Produto> produtos) {
+        List<Produto> produtoList = new ArrayList<>(produtos);
+        return produtoList.stream()
+                .sorted(Comparator.comparing(Produto::getPreco).reversed())
+                .findFirst().orElse(null);
+
+    }
+
+
+    public static List<Produto> obterProdutosPorPrecoMinimo(List<Produto> produtos, int precoMinimo) {
+        return produtos.stream()
+                .filter(produto -> produto.getPreco() >= precoMinimo)
+                .collect(Collectors.toList());
+    }
+}
