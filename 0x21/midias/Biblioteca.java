c9a8286c6ee8052e1be8cce9f5df56f6875eefac
Elton Henrique <eltonhst34@gmail.com>
2022-03-08 16:47:41 -0300

    teste

--- 0x21/midias/Biblioteca.java
+++ 0x21/midias/Biblioteca.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class Biblioteca <T>{
+public class Biblioteca <T extends Midia>{
 
     private List<T> listaGenerica = new ArrayList<>();
 
