043a36d5886a93ead91518007cde98495ae230d6
Elton Henrique <eltonhst34@gmail.com>
2022-03-14 22:23:03 -0300

    teste

--- 0x24/jar_executavel/script.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-javac -cp lib/Matematica.jar com/holbertonschool/main/Program.java
-jar -cvmf MANIFEST.MF Executavel.jar com
-java -jar Executavel.jar
--- 0x24/utilizar_jar/script.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-javac -cp lib/Matematica.jar com/holbertonschool/main/Program.java
-java -cp lib/Matematica.jar:. com.holbertonschool.main.Program
